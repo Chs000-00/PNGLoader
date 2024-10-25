10744109fbc0061001fc3f126fade5b3c7eb009b
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2024-10-25 08:16:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
-    file_path = "example.png"
+    file_path = "input.png"
+    filebytes = []
     with open(file_path, "rb") as file:
         data = file.read()
         print(data) 
+        filebytes.append(file.read(1))
+
 
 if __name__ == "__main__":
     main()
